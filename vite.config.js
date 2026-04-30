c4b2bc0305dbef4651bd738d63b0b29e6025e9b4
Abishek <abishek@example.com>
2026-04-30 08:28:08 +0530

    Update branding to Seed Neurodiverse

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
-    host: true, // ✅ important
-    allowedHosts: ['seedneurodiverse.up.railway.app']
-  },
-
-  preview: {
-    host: true, // ✅ important
-    allowedHosts: ['seedneurodiverse.up.railway.app']
+    allowedHosts: ['neurodrivers-school.vercel.app']
   }
 })
